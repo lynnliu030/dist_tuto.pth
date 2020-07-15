df61fa3aaa22f4867273528e389f4d3d437eceb3
unknown <lynnliu113@gmail.com>
2020-07-15 11:08:13 -0500

    fix

--- train_dist.py
+++ train_dist.py
@@ -87,7 +87,7 @@ def partition_dataset():
     partition = DataPartitioner(dataset, partition_sizes)
     partition = partition.use(dist.get_rank())
     train_set = torch.utils.data.DataLoader(
-        partition, batch_size=bsz, shuffle=True)
+        partition, batch_size=int(bsz), shuffle=True)
     return train_set, bsz
 
 
@@ -95,7 +95,7 @@ def average_gradients(model):
     """ Gradient averaging. """
     size = float(dist.get_world_size())
     for param in model.parameters():
-        dist.all_reduce(param.grad.data, op=dist.reduce_op.SUM, group=0)
+        dist.all_reduce(param.grad.data, op=dist.reduce_op.SUM)
         param.grad.data /= size
 
 
@@ -114,10 +114,12 @@ def run(rank, size):
         for data, target in train_set:
             data, target = Variable(data), Variable(target)
 #            data, target = Variable(data.cuda(rank)), Variable(target.cuda(rank))
+            #print("Data: ",data)
+            #print("Target: ", target)
             optimizer.zero_grad()
             output = model(data)
             loss = F.nll_loss(output, target)
-            epoch_loss += loss.data[0]
+            epoch_loss += loss.data.item()
             loss.backward()
             average_gradients(model)
             optimizer.step()
@@ -144,3 +146,4 @@ if __name__ == "__main__":
 
     for p in processes:
         p.join()
+
